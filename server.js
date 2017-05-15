f34d6ebe6688a51dae00c2faaa732473866da9f9
Boris POZZO <boris.pozzo@gmail.com>
2017-05-15 17:39:36 +0200

    add redux store

---
+++ server.js
@@ -0,0 +1,17 @@
+"use strict"
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+//middleware to define folder for static files
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'public', 'index.html'))
+});
+
+app.listen(3000, ()=> {
+  console.log('app is listening on port 3000')
+})
---
+++ src/app.js
@@ -0,0 +1,57 @@
+"use strict"
+
+import { createStore } from 'redux';
+
+//step 3 define reducers
+const reducer = (state={books:[]}, action) => {
+  switch(action.type) {
+    case "POST_BOOK":
+    //let books = state.books.concat(action.payload);
+    //return {books};
+    return {books : [...state.books, ...action.payload]}
+    break;
+    case "DELETE_BOOK":
+    const currentBookToDelete = [...state.books]
+    const indexToDelete = currentBookToDelete.findIndex(
+      (book) => {
+        book.id === action.payload.id;
+      }
+    )
+    return { books: [...currentBookToDelete.slice(0, indexToDelete),
+    ...currentBookToDelete.slice(indexToDelete + 1)]}
+    break;
+  }
+}
+
+//STEP1 create the store
+const store = createStore(reducer);
+
+store.subscribe(() => {
+  console.log('current state is ', store.getState());
+  //console.log('current price', store.getState()[1].price);
+})
+
+
+//STEP 2 create and dispatch actions
+store.dispatch({
+  type: "POST_BOOK",
+  payload: [{
+    id: 1,
+    title: 'this is the book title',
+    description: 'this is the book description',
+    price: 33.33
+  },
+  {
+    id: 2,
+    title: 'this is the second book title',
+    description: 'this is the second book description',
+    price: 50
+  }
+]
+})
+
+//dispatch second action
+store.dispatch({
+  type: "DELETE_BOOK",
+  payload: { id: 1 }
+})
