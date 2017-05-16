60f0772c9dc53d3b77c7d83cc00d292d4e7fffdb
Boris <boris.pozzo@gmail.com>
2017-05-16 08:52:08 +0200

    add UpdateBook

--- src/app.js
+++ src/app.js
@@ -5,11 +5,14 @@ import { createStore } from 'redux';
 //step 3 define reducers
 const reducer = (state={books:[]}, action) => {
   switch(action.type) {
+
+
     case "POST_BOOK":
     //let books = state.books.concat(action.payload);
     //return {books};
     return {books: [...state.books, ...action.payload]}
     break;
+
 
     case "DELETE_BOOK":
     const currentBookToDelete = [...state.books]
@@ -21,6 +24,26 @@ const reducer = (state={books:[]}, action) => {
     return {books: [...currentBookToDelete.slice(0, indexToDelete),
     ...currentBookToDelete.slice(indexToDelete + 1)]}
     break;
+
+
+    case "UPDATE_BOOK":
+    const currentBookToUpdate = [...state.books]
+    const indexToUpdate = currentBookToUpdate.findIndex(
+      function(book){
+        return book.id === action.payload.id;
+      }
+    )
+    const newBookToUpdate = {
+      ...currentBookToUpdate[indexToUpdate],
+      title: action.payload.title
+    }
+    console.log("What is it newBookToUpdate ", newBookToUpdate);
+
+    return {books: [...currentBookToUpdate.slice(0, indexToUpdate), newBookToUpdate,
+    ...currentBookToUpdate.slice(indexToUpdate + 1)]}
+    break;
+
+
   }
   return state
 }
@@ -51,8 +74,18 @@ store.dispatch({
   }]
 })
 
-//dispatch second action
+//DELETE a book
 store.dispatch({
   type:"DELETE_BOOK",
   payload: {id: 1}
+})
+
+
+//UPDATE a book
+store.dispatch({
+  type: "UPDATE_BOOK",
+  payload: {
+    id: 2,
+    title: 'Learn React in 1 months'
+  }
 })
